04bba48f3bb7702a9a6e19e3f7c7254002248f42
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-21 19:05:01 +0100

    tt

--- main.c
+++ main.c
@@ -67,6 +67,8 @@ void call(stack_t **stack, const char *instruction, int line_number)
 {
 	if (strcmp(instruction, "pall") == 0)
 		ma_pall(stack, line_number);
+	else if (strcmp(instruction, "pop") == 0)
+		ma_pop(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, instruction);
