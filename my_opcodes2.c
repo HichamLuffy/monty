1e9dfcf11cf34ca4792e56fb79378deafbcda3ec
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-21 19:48:35 +0100

    task 0

--- my_opcodes2.c
+++ my_opcodes2.c
@@ -7,13 +7,13 @@
  */
 void ma_pop(stack_t **stack, unsigned int line_number)
 {
-    stack_t *tmp;
+	stack_t *tmp;
 
-    if (stack == NULL || *stack == NULL)
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-    tmp = *stack;
-    *stack = tmp->next;
-    if (*stack)
-        (*stack)->prev = NULL;
-    free(tmp);
+	if (stack == NULL || *stack == NULL)
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+	tmp = *stack;
+	*stack = tmp->next;
+	if (*stack)
+		(*stack)->prev = NULL;
+	free(tmp);
 }
