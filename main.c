d798fb74580c90011c31584c47ab18b41811d5af
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-21 15:50:56 +0100

    task 0

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main (int argc, char *argv[])
 
     if (argc != 2)
     {
-        perror("USAGE: monty file\n");
+        fprintf(stderr,"USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
     ma_file = open_file(argv[1], "r");
