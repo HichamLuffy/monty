64d0a6b78b9c6b01022154cd3f6caa7300eddcb5
Loubna <loubna.alfidi@gmail.com>
2023-09-21 20:22:35 +0100

    pop

--- my_opcodes2.c
+++ my_opcodes2.c
@@ -9,11 +9,13 @@ void ma_pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp;
 
-	if (stack == NULL || *stack == NULL)
+	if (!*stack)
+	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
 	tmp = *stack;
-	*stack = tmp->next;
-	if (*stack)
-		(*stack)->prev = NULL;
+	*stack = (*stack)->next;
 	free(tmp);
 }
