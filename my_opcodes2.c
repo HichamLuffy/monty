bba1cfb3211e177ada841cb5eb6cb8bff10125d3
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-21 18:58:04 +0100

    task 0

--- my_opcodes2.c
+++ my_opcodes2.c
@@ -4,21 +4,22 @@
  * @stack: pointer to the top of the stack
  * @line_number: line number
  * Return: void
-*/
+ */
 void ma_pop(stack_t **stack, int line_number)
 {
-    stack_t *tmp = *stack;
-    if (!stack)
-    {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    if (!tmp->next)
-    {
-        free_stack(*stack);
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    *stack = tmp->next;
-    free(tmp);
+	stack_t *tmp = *stack;
+
+	if (!stack)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (!tmp->next)
+	{
+		free_stack(*stack);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = tmp->next;
+	free(tmp);
 }
