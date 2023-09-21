912d21a00b0bdaa78fb122e48b940fedddbac49c
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-21 18:38:31 +0100

    task 0

---
+++ my_opcodes2.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+/**
+ * ma_pop - deletes the top element of the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number
+ * Return: void
+*/
+void ma_pop(stack_t **stack, int line_number)
+{
+    stack_t *tmp = *stack;
+    if (!stack)
+    {
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    if (!tmp->next)
+    {
+        free_stack(*stack);
+        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    *stack = tmp->next;
+    free(tmp);
+}
