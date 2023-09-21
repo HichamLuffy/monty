550a8f13eff8ecc68dfc6a6b449caaa5491a8fe4
HichamLuffy <imalxvssteve2020@gmail.com>
2023-09-21 17:27:14 +0100

    task 0

--- main.c
+++ main.c
@@ -17,10 +17,14 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		eor(1);
+        exit(EXIT_FAILURE);
 	}
 	ma_file = open_file(argv[1], "r");
 	if (!ma_file)
+    {
 		eor(2, argv[1]);
+        exit(EXIT_FAILURE);
+    }
 	while (fgets(limit, LIMIT_STACK, ma_file))
 	{
 		if (strlen(limit) < 3 || limit[0] == '#')
@@ -38,12 +42,16 @@ int main(int argc, char *argv[])
 			if (!token || is_numeric(token) == 0)
 			{
 				eor(3, line_number, ma_file, stack);
+                exit(EXIT_FAILURE);
 			}
 			ma_push(&stack, atoi(token));
 		} else if (strcmp(token, "pall") == 0)
 			ma_pall(&stack, line_number);
 		else
+        {
 			eor(4, line_number, token, *stack, ma_file);
+            exit(EXIT_FAILURE);
+        }
 	}
 	free_stack(stack);
 	fclose(ma_file);
@@ -65,23 +73,23 @@ void eor(int error_code, ...)
 	{
 		case 1:
 			fprintf(stderr, "USAGE: monty file\n");
-			exit(EXIT_FAILURE);
+			break;
 		case 2:
 			fprintf(stderr, "Error: Can't open file %s\n", va_arg(ag, char *));
-			exit(EXIT_FAILURE);
+			break;
 		case 3:
 			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
 			fclose(va_arg(ag, FILE *));
 			free_stack(va_arg(ag, stack_t *));
-			exit(EXIT_FAILURE);
+			break;
 		case 4:
 			fprintf(stderr, "L%d: unknown instruction %s\n",
 					va_arg(ag, int), va_arg(ag, char *));
 			free_stack(va_arg(ag, stack_t *));
 			fclose(va_arg(ag, FILE *));
-			exit(EXIT_FAILURE);
+			break;
 		default:
-			exit(EXIT_SUCCESS);
+			break;
 	}
 	exit(EXIT_FAILURE);
 }
