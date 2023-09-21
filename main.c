87c876dfb12c2a0fa6195be58128e354ce6d2a98
Loubna <loubna.alfidi@gmail.com>
2023-09-21 18:44:53 +0100

    pint

--- main.c
+++ main.c
@@ -67,6 +67,8 @@ void call(stack_t **stack, const char *instruction, int line_number)
 {
 	if (strcmp(instruction, "pall") == 0)
 		ma_pall(stack, line_number);
+	else if (strcmp(instruction, "pint") == 0)
+          ma_pint(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, instruction);
